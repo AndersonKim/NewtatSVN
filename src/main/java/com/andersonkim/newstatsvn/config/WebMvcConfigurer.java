51126ddfffed0af553a3c1666c4bd5aabd31180d
AndersomKim <pgytao@outlook.com>
2018-11-13 00:21:09 +0800

    ok for use css and js

--- src/main/java/com/andersonkim/newstatsvn/config/WebMvcConfigurer.java
+++ src/main/java/com/andersonkim/newstatsvn/config/WebMvcConfigurer.java
@@ -4,10 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-
 @Configuration
-public class WebMvcConfigurer extends WebMvcConfigurationSupport {
+//WebMvcConfigurerAdapter：只替换指定的配置
+//WebMvcConfigurerationSupport：完全接管mvc的配置
+public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
     //配置从域名跳转到指定页面
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
